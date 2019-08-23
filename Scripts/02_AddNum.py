949a6fa462314c26961204e69474c2bbce42c2d6
Zhengting Cai <jety2858@163.com>
2019-08-23 15:06:50 +0800

    renew

---
+++ Scripts/02_AddNum.py
@@ -0,0 +1,44 @@
+import os
+from PIL import Image, ImageDraw, ImageFont
+
+class AddNum():
+    def __init__(self):
+        self.img = None
+        self.num = None
+        self.img_path = None
+
+    def open(self, img_path):
+        self.img = Image.open(img_path)
+        self.img_path = os.path.abspath(img_path)
+        return True
+
+    def draw(self, num = 1):
+        # --Initialize the number--
+        num = int(num)
+        self.num = num
+        num_str = str(num) if num < 100 else '99+'
+
+        # --Draw the number--
+        font_size = max(self.img.size[0], self.img.size[1]) // 5
+        # img_width = self.img.size[0];img_height = self.img.size[1]
+        font = ImageFont.truetype("arial.ttf", font_size)
+        text_x = self.img.size[0] - font.getsize(num_str)[0]
+        text_y = 0
+        text_color = (255, 0, 0)
+        draw = ImageDraw.Draw(self.img)
+        draw.text(
+            xy = (text_x, text_y),
+            text = num_str,
+            fill = text_color,
+            font =  font)
+
+        # --Save the new picture--
+        left, right = self.img_path.rsplit(".", 1)
+        new_img_path = left + "_" + num_str + "." + right
+        self.img.save(new_img_path)
+        return True
+
+if __name__ == '__main__':
+    solver = AddNum()
+    solver.open('./02_sample.jpg')
+    solver.draw(4)
--- Scripts/README.md
+++ Scripts/README.md
@@ -12,3 +12,10 @@
     * **2018/09/07 记录**
 
     Radiomics analysis using machine learning methods based the radiomic features extracted on the [Radcloud platform](http://mics.radcloud.cn)(Huiying Medical Technology Co.Ltd, Beijing)
+
+
+3. **Python 练习一**
+练习题目来源是["Python 练习册，每天一个小程序"](https://github.com/Yixiaohan/show-me-the-code)，部分题目代码参考如下网站：https://github.com/Show-Me-the-Code/python.
+    * **第 0000 题：** 将你的 QQ 头像（或者微博头像）右上角加上红色的数字，类似于微信未读信息数量那种提示效果。如下：
+    ![头像](http://i.imgur.com/sg2dkuY.png?1)  
+    此题主要参考：https://github.com/Show-Me-the-Code/python/blob/master/Raynxxx/0000/ex00.py 和 https://github.com/Show-Me-the-Code/python/blob/master/NeilLi1992/0000/main.py.
